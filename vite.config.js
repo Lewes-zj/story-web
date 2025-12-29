444b61e1731d9c29472063bd2ae6ebf0b9108e8b
XIN LIU <xinliu@XINdeMacBook-Pro.local>
2025-12-29 14:04:52 +0800

    add nginx

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: 6008,
+    port: 8001,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
